d69c32ca41038ba1d38cf925160dcee112c7dd37
JSHC <johan.hansson.carlberg@gmail.com>
2023-02-06 08:04:20 +0100

    Add current weather and location to custom weather object

--- src/index.js
+++ src/index.js
@@ -13,15 +13,27 @@ locationForm.addEventListener('submit', (e) => onLocationFormSubmit(e));
 })();
 
 async function getWeatherForLocation(location) {
+    const weatherObj = {};
     const locations = await getLocationFromString(location)
     .catch((err) => {
         console.log(err);
+        locationError.textContent = "Couldn't find location";
     });
+    console.log(locations);
+    weatherObj.location = {
+        country:locations[0].country,
+        lat:locations[0].lat,
+        lon:locations[0].lon,
+        name:locations[0].name
+    };
     const weather = await getWeather(locations[0].lat, locations[0].lon)
     .catch((err) => {
-
+        locationError.textContent = "Couldn't get weather for searched location";
     })
     console.log(weather);
+    weatherObj.weather = {};
+    Object.assign(weatherObj.weather,weather.main,weather.weather[0]);
+    console.log(weatherObj);
 }
 
 function onLocationFormSubmit(e) {
@@ -38,7 +50,6 @@ function onLocationFormSubmit(e) {
         await getWeatherForLocation(locationInputVal)
         .catch((err) => {
             console.log(err);
-            locationError.textContent = "Couldn't find weather for searched location";
         });
     })();
     
