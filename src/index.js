fb3c180accef1e7492ab4a80b5ad4a31c3a84b9f
JSHC <johan.hansson.carlberg@gmail.com>
2023-02-06 07:40:54 +0100

    Add initial logic for searching for weather

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import './style.css';
-
-const { getWeather, getLocationFromString } = require("./weather");
+import { getWeather, getLocationFromString } from './weather';
 
 console.log('script.js loaded');
+
+const locationForm = document.querySelector('#location-form');
+const locationInput = locationForm.querySelector('#location-input')
+const locationError = document.querySelector('#location-error');
+locationForm.addEventListener('submit', (e) => onLocationFormSubmit(e));
 
 (async () => {
     await getWeatherForLocation('Stockholm');
@@ -18,4 +22,25 @@ async function getWeatherForLocation(location) {
 
     })
     console.log(weather);
+}
+
+function onLocationFormSubmit(e) {
+    e.preventDefault();
+    locationError.textContent = '';
+    const locationInputVal = locationInput.value;
+    console.log(locationInputVal);
+    if (locationInputVal.length === 0) {
+        locationError.textContent = 'Please enter a location';
+        return;
+    }
+
+    (async () => {
+        await getWeatherForLocation(locationInputVal)
+        .catch((err) => {
+            console.log(err);
+            locationError.textContent = "Couldn't find weather for searched location";
+        });
+    })();
+    
+    
 }
