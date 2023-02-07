6835aceda390b8b3c88469278334bebea684ceae
JSHC <johan.hansson.carlberg@gmail.com>
2023-02-07 07:30:05 +0100

    Add initial render method for forecast component

--- src/forecastComponent.js
+++ src/forecastComponent.js
@@ -1,10 +1,21 @@
 export default class ForecastComponent {
-    constructor() {
+    constructor(weatherData) {
         this.element = document.createElement('div');
         this.element.classList.add('weather-forecast');
         const title = document.createElement('h3');
         title.textContent = 'Weather Forecast';
         this.element.appendChild(title);
+
+        this.renderLoading();
+        this.weatherData = weatherData;
+        this.weatherData.addListener('onLoad', () => this.onWeatherDataLoad());
+
+        return this.element;
+    }
+
+    onWeatherDataLoad() {
+        console.log('ForecastComponent weatherData loaded');
+        this.renderForecast();
     }
 
     renderLoading() {
@@ -17,5 +28,34 @@ export default class ForecastComponent {
         forecastLoadingDiv.classList.add('loading');
         forecastLoadingDiv.textContent = 'Loading forecast...';
         this.element.appendChild(forecastLoadingDiv);
+    }
+
+    renderForecast() {
+        const loadingDiv = this.element.querySelector('.loading');
+        if (loadingDiv) {
+            loadingDiv.remove();
+        } 
+
+        const forecastContentDiv = document.createElement('div');
+        forecastContentDiv.classList.add('forecast-content');
+
+        const forecastPerDay = {};
+        for (let forecastItem of this.weatherData.forecast) {
+            const itemDate = forecastItem.dt_txt.split(' ')[0];
+            if (!forecastPerDay[itemDate]) {
+                forecastPerDay[itemDate] = [];
+            }
+            forecastPerDay[itemDate].push(forecastItem);
+        }
+        console.log(forecastPerDay);
+        const forecastList = document.createElement('ul');
+        for (let key of Object.keys(forecastPerDay)) {
+            const listItem = document.createElement('li');
+            listItem.textContent = key;
+            forecastList.appendChild(listItem);
+        }
+        forecastContentDiv.appendChild(forecastList);
+
+        this.element.appendChild(forecastContentDiv);
     }
 }
