390058aa306543b7f3f022620f3f4132d1195a6f
JSHC <johan.hansson.carlberg@gmail.com>
2023-02-09 08:41:29 +0100

    Change api key to appid

--- src/weather.js
+++ src/weather.js
@@ -1,11 +1,11 @@
-const API_KEY = process.env.API_KEY;
+const APPID = process.env.APPID;
 const GEO_API_URL = 'http://api.openweathermap.org/geo/1.0/direct';
 const BASE_API_URL = 'https://api.openweathermap.org/data/2.5';
 const WEATHER_API_URL = `${BASE_API_URL}/weather`;
 const FORECAST_API_URL = `${BASE_API_URL}/forecast`
 
 export async function getLocationFromString(location) {
-    const query = `${GEO_API_URL}?q=${location}&appid=${API_KEY}`;
+    const query = `${GEO_API_URL}?q=${location}&appid=${APPID}`;
     const response = await fetch(query);
     if (!response.ok) {
         throw new Error('Error fetching location data');
@@ -15,7 +15,7 @@ export async function getLocationFromString(location) {
 }
 
 export async function getWeather(lat, lon) {
-    const query = `${WEATHER_API_URL}?lat=${lat}&lon=${lon}&units=metric&appid=${API_KEY}`;
+    const query = `${WEATHER_API_URL}?lat=${lat}&lon=${lon}&units=metric&appid=${APPID}`;
     const response = await fetch(query);
     if (!response.ok) {
         throw new Error('Error fetching weather data');
@@ -25,7 +25,7 @@ export async function getWeather(lat, lon) {
 }
 
 export async function getWeatherForecast(lat, lon) {
-    const query = `${FORECAST_API_URL}?lat=${lat}&lon=${lon}&units=metric&appid=${API_KEY}`;
+    const query = `${FORECAST_API_URL}?lat=${lat}&lon=${lon}&units=metric&appid=${APPID}`;
     const response = await fetch(query);
     if (!response.ok) {
         throw new Error('Error fetching weather forecast data');
